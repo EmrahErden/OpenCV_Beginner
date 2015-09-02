23e79b54c49f26257011ada83f90219fac38aea9
SoajanII <emraherden@gmail.com>
2015-09-03 00:03:36 +0300

    histogram calculation and display for single channel images

--- simpleHistogram.cpp
+++ simpleHistogram.cpp
@@ -1,27 +1,59 @@
 //g++ simpleHistogram.cpp `pkg-config --libs opencv` -o simpleHistogram
 
+/*
+Modified: http://docs.opencv.org/3.0-beta/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html
+for single channel images
+*/
+
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 
 //histogram display for gray-scale images
-Mat computeGSHist(Mat & input)
+void computeGSHist(Mat & input)
 {
-	const int * channel_numbers = { 0 };
-	float channel_range[] = { 0.0, 255.0 };
-	const float * channel_ranges = channel_range;
-	int number_bins = 64;
-	Mat histogram;
-	calcHist( &input, 1, channel_numbers, Mat(), histogram,
-	1, &number_bins, &channel_ranges);
+  	/// Establish the number of bins
+  	int histSize = 256;
 
-	return histogram;
+  	/// Set the ranges ( for B,G,R) )
+  	float range[] = { 0, 256 } ;
+  	const float* histRange = { range };
+
+  	bool uniform = true; bool accumulate = false;
+
+  	Mat hist;
+
+  	/// Compute the histograms:
+  	calcHist(&input, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate );
+
+	// Draw the histograms for B, G and R
+	int hist_w = 512; int hist_h = 400;
+	int bin_w = cvRound( (double) hist_w/histSize );
+
+	Mat histImage( hist_h, hist_w, CV_8UC1, Scalar(0) );
+
+	/// Normalize the result
+	normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+ 	for( int i = 1; i < histSize; i++ )
+	{
+		line( histImage, Point( bin_w*(i-1), hist_h - cvRound(hist.at<float>(i-1)) ) ,
+		Point( bin_w*(i), hist_h - cvRound(hist.at<float>(i)) ), Scalar( 255));
+	}
+
+	/// Display
+	namedWindow("calcHist Demo", WINDOW_AUTOSIZE );
+	imshow("calcHist Demo", histImage );
+
+	waitKey(0);
 }
 
 int main(void)
 {
 	Mat img = imread("sample.jpeg");
 	Mat imgGS, hist;
-	img.convertTo(imgGS, CV_8UC3);
-	hist = computeGSHist(imgGS);
+	cvtColor(img, imgGS, COLOR_BGR2GRAY);
+	computeGSHist(imgGS);
 }
+
